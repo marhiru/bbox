24bc1f2fc374c21fcc578e04a9888b843d363d00
marhiru <marcosgamern@gmail.com>
2025-08-12 16:06:55 -0300

    feat: added an avatar to the navigation

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,11 +1,17 @@
 import { JSX } from "react";
 import { ThemeSwitcher } from "./theme";
+import { Avatar, AvatarImage } from "./ui/avatar";
 
 export default function Navigation(): JSX.Element {
   return (
-    <nav className="flex bg-background gap-2 justify-between mx-auto items-center h-9 w-full sticky">
+    <nav className="flex bg-background/60 backdrop-blur-md gap-2 py-6 px-4 justify-between mx-auto items-center h-9 w-full fixed">
       <div>aspodksop asdsa</div>
-      <ThemeSwitcher />
+      <div className="flex gap-2 items-center justify-center">
+        <ThemeSwitcher />
+        <Avatar>
+          <AvatarImage src="https://avatars.githubusercontent.com/u/66804488?v=4" />
+        </Avatar>
+      </div>
     </nav>
   );
 }
