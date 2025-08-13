1ea2aba939e7103394f215bf08c3f5ffd45bde8c
marhiru <marcosgamern@gmail.com>
2025-08-13 14:34:17 -0300

    fix: fixed styling of our theme

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -5,10 +5,12 @@ import { Avatar, AvatarImage } from "./ui/avatar";
 export default function Navigation(): JSX.Element {
   return (
     <nav className="flex bg-background/60 backdrop-blur-md gap-2 py-6 px-4 justify-between mx-auto items-center h-9 w-full fixed">
-      <div>aspodksop asdsa</div>
+      <div className="font-mono select-none font-semibold bg-gradient-to-br from-primary via-90% to-primary/50 ring-0 hover:ring-2 ring-offset-0 hover:ring-offset-2 ring-offset-background ring-primary transition duration-[82.5ms] px-2 rounded-md">
+        b_box
+      </div>
       <div className="flex gap-2 items-center justify-center">
         <ThemeSwitcher />
-        <Avatar>
+        <Avatar className="ring-2 ml-2 ring-primary ring-offset-2 ring-offset-background">
           <AvatarImage src="https://avatars.githubusercontent.com/u/66804488?v=4" />
         </Avatar>
       </div>
