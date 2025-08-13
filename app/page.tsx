eccb722a4b7fa03956cebcf0edac3f2ff93091ef
marhiru <marcosgamern@gmail.com>
2025-08-13 14:33:51 -0300

    fix: added a image to test our annotator component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
-import { Rectangle } from "@/components/rect";
+
+import Test from "@/app/2k.jpg"
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="font-inter bg-background grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        {/* <div className="flex gap-4 items-center flex-col sm:flex-row">
           <Rectangle />
-        </div>
-        <div className="flex h-screen gap-4 items-center flex-col sm:flex-row">
-          asdijkoasdkiosadkop
+        </div> */}
+        <div className="flex gap-4 border items-center flex-col sm:flex-row">
+          <Image src={Test} alt="all" />
+          
         </div>
       </main>
     </div>
