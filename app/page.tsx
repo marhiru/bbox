cbb58c1afc3bfddc0633a956694ce8cd5c88149b
marhiru <marcosgamern@gmail.com>
2025-08-13 23:05:32 -0300

    feat: added json visualization

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 
   return (
     <div className="font-inter bg-background grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex flex-1 gap-8 row-start-2 items-center sm:items-start">
         <Annotator
           url="https://avatars.githubusercontent.com/u/66804488?v=4"
           inputMethod="select"
@@ -22,10 +22,22 @@ export default function Home() {
           className="border"
           onChange={handleEntriesChange}
         />
+        <pre
+          className="bg-muted w-64 overflow-clip h-64 overflow-y-auto"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(entries, null, 2)
+              .replace(/"(.*?)":/g, '<span style="color: #c678dd">"$1"</span>:')
+              .replace(/: "([^"]*)"/g, ': <span style="color: #98c379">"$1"</span>')
+              .replace(/: (\d+)/g, ': <span style="color: #61afef">$1</span>')
+              .replace(/true|false/g, (match) =>
+                `<span style="color: #e06c75">${match}</span>`
+              )
+              .replace(/null/g, '<span style="color: #d19a66">null</span>')
+              .replace(/\n/g, "<br/>")
+              .replace(/  /g, "&nbsp;&nbsp;"),
+          }}
+        />
       </main>
-      {/* <pre>
-        {JSON.stringify(entries)}
-      </pre> */}
     </div>
   );
 }
